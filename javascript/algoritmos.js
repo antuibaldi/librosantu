66e44a93b64d2c2a7a4be1ab9e6b6f706dc5fde2
Antu <122485403+antuibaldi@users.noreply.github.com>
2024-06-19 23:44:04 -0300

    commit-20-06

--- javascript/algoritmos.js
+++ javascript/algoritmos.js
@@ -1,3 +1,3 @@
 function botonAlert() {
-    alert ("SI QUIERE CONTRIBUIR AL PROYECTO DORNAR A ALIAS:antu.ibaldi");
+    swal ("SI QUIERE CONTRIBUIR AL PROYECTO DONAR A"," ALIAS:antu.ibaldi o ALIAS:24371980950.BNC1");
 }
